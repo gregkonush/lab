93cea8d38edb03cf9e1f954014bd5ed6b5c5103c
Greg Konush <gkonushev@outlook.com>
2024-03-30 11:19:39 -0700

    ✨ Add cloud-init config for Ubuntu in Harvester
    Introduce cloud-init configuration for Ubuntu virtual machine in Harvester.
    Includes setting up Docker, SSH keys, user groups, and more.

--- terraform/harvester/main.tf
+++ terraform/harvester/main.tf
@@ -51,7 +51,6 @@ resource "harvester_virtualmachine" "rancher2" {
     image       = harvester_image.ubuntu.id
   }
 
-
   network_interface {
     name           = "bridge"
     model          = "virtio"
@@ -59,6 +58,51 @@ resource "harvester_virtualmachine" "rancher2" {
     network_name   = harvester_network.cluster_network.name
     wait_for_lease = true
   }
+
+  cloudinit {
+    user_data_secret_name    = harvester_cloudinit_secret.ubuntu-cloud-config.name
+    network_data_secret_name = harvester_cloudinit_secret.ubuntu-cloud-config.name
+  }
 }
 
+resource "harvester_cloudinit_secret" "ubuntu-cloud-config" {
+  name = "ubuntu-cloud-config"
 
+  user_data    = <<-EOF
+#cloud-config
+package_update: true
+package_upgrade: true
+package_reboot_if_required: true
+packages:
+  - qemu-guest-agent
+runcmd:
+  - systemctl enable --now qemu-guest-agent
+  - install -m 0755 -d /etc/apt/keyrings
+  - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  - chmod a+r /etc/apt/keyrings/docker.gpg
+  - |
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
+  - apt-get update
+  - apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  - systemctl enable --now docker
+  - echo 'alias k=kubectl' >> /home/kalmyk/.bashrc
+  - mkdir -p /home/kalmyk/.ssh && chmod 700 /home/kalmyk/.ssh
+  - touch /home/kalmyk/.ssh/authorized_keys && chmod 600 /home/kalmyk/.ssh/authorized_keys
+  - curl https://github.com/gregkonush.keys -o /home/kalmyk/.ssh/authorized_keys
+groups:
+  - docker
+users:
+  - name: kalmyk
+    groups: [adm, cdrom, dip, plugdev, lxd, sudo, docker]
+    lock_passwd: false
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    no_ssh_fingerprints: false
+    ssh:
+      emit_keys_to_console: false
+EOF
+  network_data = ""
+}
