6c095f7e7e4767a07e6d0330111a190a026f038b
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-04-04 21:26:43 -0700

    🔄 Updated Terraform provider versions and added backend configuration

--- terraform/rancher/main.tf
+++ terraform/rancher/main.tf
@@ -1,4 +1,17 @@
+terraform {
+  required_providers {
+    rancher2 = {
+      source  = "rancher/rancher2"
+      version = ">= 4.1.0"
+    }
+  }
+
+  backend "pg" {
+    conn_str = "postgres://altra:@nuc.lan:5432/altra?sslmode=disable"
+  }
+}
 provider "rancher2" {
-  api_url   = "asdf"
+  api_url   = "https://rancher.lan"
   bootstrap = true
+  insecure = true
 }
