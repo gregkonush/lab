c59bbabe8ce211b21b04d8abcf15bb813df5fb16
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-04-20 20:00:37 -0700

    ✨ Add Ecran frontend project with Tailwind CSS and Next.js support (#68)
    - Added Ecran frontend project to the workspace with Tailwind CSS for styling
    - Integrated support for Next.js to build and run the project efficiently
    - Included necessary dependencies and configurations for the project

---
+++ apps/ecran/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <header className="flex items-center text-2xl px-5 py-4 border-b border-gray-800 uppercase">
+        proompteng<span className="text-indigo-400">▪</span>ai
+      </header>
+    </main>
+  )
+}
