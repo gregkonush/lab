f8c3a54476b6fa1a413265691a441a49c55436e6
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-04-22 13:11:27 -0700

    ✨ Add sign up form on homepage
    Added a sign up form on the homepage for users to join the waitlist.

--- apps/ecran/src/app/page.tsx
+++ apps/ecran/src/app/page.tsx
@@ -4,6 +4,15 @@ export default function Home() {
       <header className="flex items-center text-2xl px-5 py-4 border-b border-gray-800 uppercase">
         proompteng<span className="text-indigo-400">▪</span>ai
       </header>
+      <div className="mt-40 flex flex-col items-center justify-center space-y-2">
+        <div>sign up for waitlist</div>
+        <form>
+          <input className="mx-2 rounded px-2 py-0.5 text-indigo-800/80" type="email" placeholder="your email" />
+          <button className="bg-indigo-500 text-white px-2 py-0.5 rounded" type="submit">
+            sign up
+          </button>
+        </form>
+      </div>
     </main>
   )
 }
