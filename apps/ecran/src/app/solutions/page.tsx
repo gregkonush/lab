a7c46165686f6bc23dbf7bdeac254dd0d0cbba2f
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-08-23 14:36:45 -0700

    🐳 Added PostgreSQL service to docker-compose.yaml. (#178)
    * 🐳 Added PostgreSQL service to docker-compose.yaml.
    
    * 🔧 Add logging

--- apps/ecran/src/app/solutions/page.tsx
+++ apps/ecran/src/app/solutions/page.tsx
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 
 export default async function Solutions() {
-  console.log(process.env)
   return (
     <div className="prose dark:prose-invert py-16">
       <h2>Solutions</h2>
