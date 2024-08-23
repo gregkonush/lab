c041459a9408d38ed66f778a67bb0b1b2bd095cc
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-08-22 17:34:16 -0700

    Add leetcode solutions page

--- apps/ecran/next.config.mjs
+++ apps/ecran/next.config.mjs
@@ -1,6 +1,13 @@
+import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: 'standalone',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
---
+++ apps/ecran/src/app/solutions/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Solutions() {
+  return (
+    <div className="prose dark:prose-invert py-16">
+      <h2>Solutions</h2>
+      <Link href="/solutions/python/next-permutation">Next Permutation</Link>
+    </div>
+  )
+}
