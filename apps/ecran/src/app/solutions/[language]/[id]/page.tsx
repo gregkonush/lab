a83bc9ecc2bc1a295eaf47751e6431015174b392
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-08-23 11:48:20 -0700

    feat: Add syntax highlighting (#176)

---
+++ apps/ecran/src/app/solutions/[language]/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import rehypeHighlight from 'rehype-highlight'
+import '@/styles/tokyo-night-dark.css'
+import fs from 'node:fs'
+
+export default function Home() {
+  const source = fs.readFileSync(process.cwd() + '/src/solutions/next-permutation.md', 'utf8')
+  return (
+    <div className="bg-[#1a1b26] rounded-lg">
+      <MDXRemote
+        source={source}
+        options={{
+          mdxOptions: {
+            remarkPlugins: [],
+            rehypePlugins: [rehypeHighlight],
+          },
+        }}
+      />
+    </div>
+  )
+}
