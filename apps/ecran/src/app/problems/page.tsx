de91aa95eab60ec00b78429ea8b44a568709e15f
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-08-28 23:56:51 -0700

    🚀  Render problem details page. (#220)

--- apps/ecran/src/app/problems/page.tsx
+++ apps/ecran/src/app/problems/page.tsx
@@ -3,22 +3,26 @@ import { Textarea } from '@/components/ui/textarea'
 import { createProblem } from './actions'
 import { Input } from '@/components/ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { ComboSelect } from '@/components/combo-select'
 
 export default async function Problems() {
   return (
     <form action={createProblem} className="space-y-4 min-w-full">
       <Button size="sm">Save</Button>
       <Input name="title" placeholder="Problem Name..." />
-      <Select name="difficulty">
-        <SelectTrigger>
-          <SelectValue placeholder="Difficulty" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="easy">Easy</SelectItem>
-          <SelectItem value="medium">Medium</SelectItem>
-          <SelectItem value="hard">Hard</SelectItem>
-        </SelectContent>
-      </Select>
+      <div className="flex flex-row space-x-4">
+        <Select name="difficulty">
+          <SelectTrigger className="w-36">
+            <SelectValue placeholder="Difficulty" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="easy">Easy</SelectItem>
+            <SelectItem value="medium">Medium</SelectItem>
+            <SelectItem value="hard">Hard</SelectItem>
+          </SelectContent>
+        </Select>
+        <ComboSelect />
+      </div>
       <Textarea name="description" placeholder="Paste your description here..." className="min-h-96" />
     </form>
   )
