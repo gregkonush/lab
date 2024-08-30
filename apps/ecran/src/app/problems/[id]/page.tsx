15720b8dcacceb6693786a9cecf22a9f879f52fd
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-08-29 23:30:12 -0700

    ✨ Add problems list page

--- apps/ecran/src/app/problems/[id]/page.tsx
+++ apps/ecran/src/app/problems/[id]/page.tsx
@@ -1,10 +1,38 @@
 import { logger } from '@/utils/logger'
 import { db } from '@/db'
 import { problems } from '@/db/schema'
+import { Textarea } from '@/components/ui/textarea'
+import { createProblem } from './actions'
+import { Input } from '@/components/ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { ComboSelect } from '@/components/combo-select'
+import { Button } from '@/components/ui/button'
 import { eq } from 'drizzle-orm'
 
 export default async function Problem({ params: { id } }: { params: { id: string } }) {
   logger.info('Loading problem', { id })
+  if (id === 'create') {
+    return (
+      <form action={createProblem} className="space-y-4 min-w-full">
+        <Button size="sm">Save</Button>
+        <Input name="title" placeholder="Problem Name..." />
+        <div className="flex flex-row space-x-4">
+          <Select name="difficulty">
+            <SelectTrigger className="w-36">
+              <SelectValue placeholder="Difficulty" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="easy">Easy</SelectItem>
+              <SelectItem value="medium">Medium</SelectItem>
+              <SelectItem value="hard">Hard</SelectItem>
+            </SelectContent>
+          </Select>
+          <ComboSelect />
+        </div>
+        <Textarea name="description" placeholder="Paste your description here..." className="min-h-96" />
+      </form>
+    )
+  }
   const problem = await db.select().from(problems).where(eq(problems.id, id))
 
   return (
---
+++ apps/ecran/src/app/problems/columns.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { ColumnDef } from '@tanstack/react-table'
+import Link from 'next/link'
+
+type Problem = {
+  id: string
+  title: string
+  difficulty: string
+  tags: string[]
+}
+export const columns: ColumnDef<Problem>[] = [
+  {
+    accessorKey: 'title',
+    header: 'Title',
+    cell: ({
+      row: {
+        original: { id, title },
+      },
+    }) => <Link href={`/problems/${id}`}>{title}</Link>,
+  },
+  {
+    accessorKey: 'difficulty',
+    header: 'Difficulty',
+  },
+  {
+    accessorKey: 'tags',
+    header: 'Tags',
+  },
+]
