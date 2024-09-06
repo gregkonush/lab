c302b11aabeef4bfeb96506c36802853aecb56e6
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-09-05 23:36:01 -0700

    feat: Add example of temporal execution (#234)

---
+++ apps/ecran/src/app/api/startBuy/route.ts
@@ -0,0 +1,32 @@
+import { oneClickBuy } from '@temporal/workflows';
+import { getTemporalClient } from '@temporal/client';
+import { TASK_QUEUE_NAME } from '@temporal/shared';
+
+export async function POST(req: Request) {
+  interface RequestBody {
+    itemId: string;
+    transactionId: string;
+  }
+
+  let body: RequestBody;
+
+  try {
+    body = await req.json() as RequestBody;
+  } catch (error) {
+    return new Response("Invalid JSON body", { status: 400 });
+  }
+
+  const { itemId, transactionId } = body;
+
+  if (!itemId) {
+    return new Response("Must send the itemID to buy", { status: 400 });
+  }
+
+  await getTemporalClient().workflow.start(oneClickBuy, {
+    taskQueue: TASK_QUEUE_NAME,
+    workflowId: transactionId,
+    args: [itemId],
+  });
+
+  return Response.json({ ok: true });
+}
---
+++ apps/ecran/temporal/src/workflows.ts
@@ -0,0 +1,13 @@
+import { proxyActivities, sleep } from '@temporalio/workflow';
+import type * as activities from './activities';
+
+const { purchase } = proxyActivities<typeof activities>({
+  startToCloseTimeout: '1 minute',
+});
+
+export async function oneClickBuy(id: string): Promise<string> {
+  const result = await purchase(id); // calling the activity
+  await sleep('10 seconds'); // sleep to simulate a longer response.
+  console.log(`Activity ID: ${result} executed!`);
+  return result;
+}
