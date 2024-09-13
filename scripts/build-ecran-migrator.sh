795dd15fd542c20f5ac82da8d83447fcd06db3b7
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-09-12 23:26:08 -0700

    feat(ecran): Separation between workers, main and migrator (#294)

---
+++ scripts/build-ecran-migrator.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set variables
+IMAGE_NAME="gitea.proompteng.ai/lab/ecran-migrator"
+DOCKERFILE="apps/ecran/Dockerfile.migrator"
+CONTEXT_PATH="apps/ecran"
+
+# Check if a tag is provided as an argument
+if [ $# -eq 1 ]; then
+    TAG=$1
+else
+    # If no tag is provided, use the current date and time
+    TAG=$(date +"%Y%m%d_%H%M%S")
+fi
+
+# Full image name with tag
+FULL_IMAGE_NAME="${IMAGE_NAME}:${TAG}"
+
+# Build the Docker image
+echo "Building Docker image: ${FULL_IMAGE_NAME}"
+docker buildx build --platform linux/arm64 -t ${FULL_IMAGE_NAME} -f ${DOCKERFILE} ${CONTEXT_PATH} --push
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image built and pushed successfully: ${FULL_IMAGE_NAME}"
+else
+    echo "Docker image build or push failed"
+    exit 1
+fi
--- scripts/docker_build_push.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker buildx build --platform linux/arm64 -t gitea.proompteng.ai/lab/ecran:latest ./apps/ecran --file ./apps/ecran/Dockerfile --push
