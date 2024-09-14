b77efc8303cf17582daa4fa5a15a958cfb5171a0
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-09-13 20:15:43 -0700

    feat(ecran): Update worker container and styling

--- apps/ecran/src/app/page.tsx
+++ apps/ecran/src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
 
   return (
     <div className="flex min-w-full min-h-[calc(100vh-10rem)] flex-col items-center justify-center prose dark:prose-invert">
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-sky-500">AI Leetcode Coach</h1>
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-500 to-sky-500 animate-gradient-x">AI Interview Coach</h1>
       <p className="text-left">Tech interview practice made easy.</p>
     </div>
   )
