33a8481b40d129403af2957c8769e76e9ff74dd3
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-09-13 20:15:56 -0700

    feat(ecran): Update worker container and styling (#305)

--- apps/ecran/tailwind.config.ts
+++ apps/ecran/tailwind.config.ts
@@ -65,10 +65,21 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'gradient-x': 'gradient-x 10s ease infinite',
       },
     },
   },
