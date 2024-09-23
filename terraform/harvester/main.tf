b7158efe7bb132ceafc86bc617086c9cbaaf5913
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-09-22 17:34:03 -0700

    fix: disable fstrim in Ubuntu cloud-init (#347)

--- terraform/harvester/main.tf
+++ terraform/harvester/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     harvester = {
       source  = "harvester/harvester"
-      version = ">= 0.6.4"
+      version = "= 0.6.4"
     }
   }
   backend "pg" {
@@ -18,39 +18,63 @@ locals {
   vms = {
     "kube-master-00" = {
       mac_address = "00:16:3E:3C:0E:FC" // 192.168.1.150
+      cpu         = 10
+      memory      = "16Gi"
     }
     "kube-master-01" = {
       mac_address = "00:16:3E:3C:0E:FD" // 192.168.1.151
+      cpu         = 10
+      memory      = "16Gi"
     }
     "kube-master-02" = {
       mac_address = "00:16:3E:3C:0E:FE" // 192.168.1.152
+      cpu         = 10
+      memory      = "16Gi"
     }
     "kube-worker-00" = {
       mac_address = "00:16:3E:3C:0E:FF" // 192.168.1.160
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-01" = {
       mac_address = "00:16:3E:3C:0E:00" // 192.168.1.161
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-02" = {
       mac_address = "00:16:3E:3C:0E:01" // 192.168.1.162
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-03" = {
       mac_address = "00:16:3E:3C:0E:02" // 192.168.1.163
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-04" = {
       mac_address = "00:16:3E:3C:0E:03" // 192.168.1.164
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-05" = {
       mac_address = "00:16:3E:3C:0E:04" // 192.168.1.165
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-06" = {
       mac_address = "00:16:3E:3C:0E:05" // 192.168.1.166
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-07" = {
       mac_address = "00:16:3E:3C:0E:06" // 192.168.1.167
+      cpu         = 10
+      memory      = "8Gi"
     }
     "kube-worker-08" = {
       mac_address = "00:16:3E:3C:0E:07" // 192.168.1.168
+      cpu         = 10
+      memory      = "8Gi"
     }
   }
 }
@@ -61,6 +85,10 @@ resource "harvester_image" "ubuntu-focal" {
   source_type  = "download"
   namespace    = "harvester-public"
   url          = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-arm64.img"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "harvester_image" "ubuntu-jammy" {
@@ -69,10 +97,29 @@ resource "harvester_image" "ubuntu-jammy" {
   source_type  = "download"
   namespace    = "harvester-public"
   url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-arm64.img"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 data "harvester_clusternetwork" "mgmt" {
   name = "mgmt"
+}
+
+resource "harvester_storageclass" "single-node-longhorn" {
+  name               = "single-node-longhorn"
+  volume_provisioner = "driver.longhorn.io"
+  is_default         = true
+
+  parameters = {
+    "migratable"          = "true"
+    "numberOfReplicas"    = "1"
+    "staleReplicaTimeout" = "30"
+  }
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = true
+  volume_binding_mode    = "Immediate"
 }
 
 resource "harvester_network" "cluster_network" {
@@ -84,21 +131,20 @@ resource "harvester_network" "cluster_network" {
 resource "harvester_virtualmachine" "kube-cluster" {
   for_each = local.vms
   name     = each.key
-  cpu      = 4
-  memory   = "8Gi"
+  cpu      = each.value.cpu
+  memory   = each.value.memory
 
   efi          = true
   hostname     = each.key
   run_strategy = "Always"
 
   disk {
-    name        = "root"
-    type        = "disk"
-    size        = "100Gi"
-    bus         = "virtio"
-    auto_delete = true
-    boot_order  = 1
-    image       = harvester_image.ubuntu-jammy.id
+    name       = "root"
+    type       = "disk"
+    size       = "200Gi"
+    bus        = "virtio"
+    boot_order = 1
+    image      = harvester_image.ubuntu-jammy.id
   }
 
   network_interface {
@@ -116,6 +162,7 @@ resource "harvester_virtualmachine" "kube-cluster" {
   }
 }
 
+# https://github.com/harvester/harvester/issues/4739 ~ Disable fstrim
 resource "harvester_cloudinit_secret" "ubuntu-plain" {
   name = "ubuntu-plain"
 
@@ -124,10 +171,28 @@ resource "harvester_cloudinit_secret" "ubuntu-plain" {
 package_update: true
 package_upgrade: true
 package_reboot_if_required: true
+bootcmd:
+  - sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&discard=off /' /etc/default/grub
+  - update-grub
 packages:
   - qemu-guest-agent
+write-files:
+  - path: /etc/sysctl.d/60-nvme-optimizations.conf
+    content: |
+      vm.max_map_count = 262144
+      fs.file-max = 65536
+      vm.swappiness = 10
+  - path: /etc/udev/rules.d/60-scheduler-nvme.rules
+    content: |
+      # Set I/O scheduler to 'none' for NVMe devices
+      ACTION=="add|change", KERNEL=="nvme[0-9]n[0-9]", ATTR{queue/scheduler}="none"
 runcmd:
   - systemctl enable --now qemu-guest-agent
+  - systemctl mask fstrim.timer
+  - systemctl stop fstrim.timer
+  - sysctl -p /etc/sysctl.d/60-nvme-optimizations.conf
+  - udevadm control --reload-rules
+  - udevadm trigger
 users:
   - name: kalmyk
     groups: [adm, cdrom, dip, plugdev, lxd, sudo]
