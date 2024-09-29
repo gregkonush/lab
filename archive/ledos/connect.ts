c3a6de6f68863e35d6b3eaef1bf3e2d5d1cb8455
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-09-29 00:03:31 -0700

    feat(cert-manager): added cert-manager, clean up code

--- archive/ledos/connect.ts
+++
@@ -1,9 +0,0 @@
-import type { ConnectRouter } from "@connectrpc/connect"
-import { ProomptService } from "./generated/ai/proompteng/ledos/v1/proompt_api_connect"
-
-export default (router: ConnectRouter) =>
-  router.service(ProomptService, {
-    query: async function* query(req) {
-      yield { answer: `Let me find answer for ~ ${req.input}` }
-    },
-  })
