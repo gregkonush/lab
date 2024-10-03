b76388c563a5f278eb4a12d490c80da62f4c964f
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-10-03 01:29:33 -0700

    fix: remove ignore block

--- packages/ecran/src/app/problems/[id]/page.tsx
+++ packages/ecran/src/app/problems/[id]/page.tsx
@@ -1,4 +1,4 @@
-import 'highlight.js/styles/github-dark.css' // Add this import at the top of the file
+import 'highlight.js/styles/github-dark.css'
 import { logger } from '@/utils/logger'
 import { db } from '@/db'
 import { problems, solutions } from '@/db/schema'
