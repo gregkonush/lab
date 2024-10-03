85820f843138560d63a3804fe99d68fb108171d2
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-10-03 01:20:56 -0700

    feat: try deno and some changes

--- packages/ecran/src/app/practice/page.tsx
+++ packages/ecran/src/app/practice/page.tsx
@@ -6,9 +6,9 @@ export default async function Practice() {
   return (
     <div className="flex flex-row h-[calc(100vh-70px)] w-full p-4">
       <div className="basis-1/2">
-        <div className="p-4">
+        <div className="p-4 space-y-4">
           <h1 className="text-2xl font-bold">Practice Area</h1>
-          <p>Instructions or other content can go here.</p>
+          <div>Select a topic that you want to work on</div>
         </div>
       </div>
       <div className="basis-1/2 shrink-0">
