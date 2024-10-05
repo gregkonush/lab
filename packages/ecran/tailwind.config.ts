7eda1bb7a684e0d5d0b73162f5710e7e50aa57a1
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-10-05 15:26:28 -0700

    patch: add shadow to a button (#461)

--- packages/ecran/tailwind.config.ts
+++ packages/ecran/tailwind.config.ts
@@ -17,6 +17,10 @@ const config = {
       fontFamily: {
         mono: ['var(--font-jetbrains-mono)'],
       },
+      boxShadow: {
+        'dark-sm': '0 1px 2px 0 rgba(255, 255, 255, 0.05)',
+        'dark-md': '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)',
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
