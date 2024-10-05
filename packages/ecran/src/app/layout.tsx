0d035c6edae52f2bb17c1d20898bc0a48449558e
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-10-04 20:35:17 -0700

    feat: add a version in the header (#457)

--- packages/ecran/src/app/layout.tsx
+++ packages/ecran/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as k8s from '@kubernetes/client-node'
 import type { Metadata } from 'next'
 import { JetBrains_Mono } from 'next/font/google'
 import Link from 'next/link'
@@ -13,16 +14,14 @@ export const metadata: Metadata = {
     'ProomptEng.AI is the largest, most trusted online community for developers to learn, share their prompt engineering knowledge.',
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const version = await getAppImageVersion()
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${jetbrains.variable} font-mono`} suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <header className="flex items-center space-x-10 px-10 py-4 border-b border-gray-800">
+          <header className="flex items-center space-x-10 px-10 py-4 border-b border-gray-800 justify-center">
             <div className="text-2xl uppercase">
               <Link href="/">
                 proompteng<span className="text-indigo-400">▪</span>ai
@@ -37,10 +36,37 @@ export default function RootLayout({
             <Link href="/practice" className="hover:underline">
               Practice
             </Link>
+            <div className="justify-end">Version: {version}</div>
           </header>
           {children}
         </ThemeProvider>
       </body>
     </html>
   )
+}
+
+async function getAppImageVersion() {
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+
+  const k8sApi = kc.makeApiClient(k8s.AppsV1Api)
+  const namespace = process.env.NAMESPACE || 'ecran'
+
+  try {
+    const res = await k8sApi.readNamespacedDeployment('ecran', namespace)
+    const containers = res.body.spec?.template?.spec?.containers
+
+    if (containers && containers.length > 0) {
+      const ecranContainer = containers.find((c) => c.name === 'ecran')
+      if (ecranContainer) {
+        const image = ecranContainer.image
+        const version = image?.split(':')?.[1] || 'unknown'
+        return version
+      }
+    }
+    return 'unknown'
+  } catch (err) {
+    console.error('Error fetching deployment:', err)
+    return 'unknown'
+  }
 }
