491bd875995956aa48e00cabb0634e5e76a4bc93
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-10-11 00:46:49 -0700

    feat: update text styles and invalidate root layout on sign-in (#477)

--- services/ecran/src/app/page.tsx
+++ services/ecran/src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <div className="flex min-w-full min-h-[calc(100vh-10rem)] flex-col items-center justify-center prose dark:prose-invert">
       <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-500 to-sky-500 animate-gradient-x">AI Interview Coach</h1>
-      <p className="text-left">Tech interview practice made easy.</p>
+      <p className="text-left text-lg">Tech interview practice made easy.</p>
       <div className="w-64 h-64 mt-8">
         <ThreeScene />
       </div>
