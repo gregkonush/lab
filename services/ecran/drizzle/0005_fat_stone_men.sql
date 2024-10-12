d2c9073d887a0dfc76ad02ab642e3d03d629595e
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-10-11 19:47:33 -0700

    feat: save user id with executions (#480)

---
+++ services/ecran/drizzle/0005_fat_stone_men.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "executions" ADD COLUMN "user_id" uuid;--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "executions" ADD CONSTRAINT "executions_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
