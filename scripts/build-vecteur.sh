e4356d628cf08f3a5fea4d0329da7e4b712b3916
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-11-03 15:46:42 -0800

    feat: add pgvecto.rs to a cluster

---
+++ scripts/build-vecteur.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set variables
+IMAGE_NAME="kalmyk.duckdns.org/lab/vecteur"
+DOCKERFILE="services/vecteur/Dockerfile"
+CONTEXT_PATH="services/vecteur"
+
+# Check if a tag is provided as an argument
+if [ $# -eq 1 ]; then
+    TAG=$1
+else
+    # If no tag is provided, use the current date and time
+    TAG=$(date +"%Y%m%d_%H%M%S")
+fi
+
+# Full image name with tag
+FULL_IMAGE_NAME="${IMAGE_NAME}:${TAG}"
+
+# Build the Docker image
+echo "Building Docker image: ${FULL_IMAGE_NAME}"
+docker buildx build \
+    --platform linux/arm64 \
+    --build-arg PGVECTORS_TAG=v0.3.0 \
+    --build-arg TARGETARCH=arm64 \
+    -t ${FULL_IMAGE_NAME} \
+    -f ${DOCKERFILE} ${CONTEXT_PATH} \
+    --push
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image built and pushed successfully: ${FULL_IMAGE_NAME}"
+else
+    echo "Docker image build or push failed"
+    exit 1
+fi
