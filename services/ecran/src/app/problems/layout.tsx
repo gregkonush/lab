aea9180a23caef91f6c4fdf7e3ada3c60f9207a1
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2024-11-14 00:10:01 -0800

    feat: small tweaks to landing (#551)

--- services/ecran/src/app/problems/layout.tsx
+++ services/ecran/src/app/problems/layout.tsx
@@ -1,3 +1,3 @@
 export default async function ProblemsLayout({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col">{children}</div>
+  return <div className="flex flex-col px-10">{children}</div>
 }
