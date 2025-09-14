a09596f5c9d85566205f5e16de04b27c60b56cf1
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2025-09-14 16:40:37 -0700

    reinstall

--- apps/proompteng/src/components/ui/pagination.tsx
+++ apps/proompteng/src/components/ui/pagination.tsx
@@ -1,12 +1,11 @@
-import * as React from "react";
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
   MoreHorizontalIcon,
 } from "lucide-react";
-
+import type * as React from "react";
+import { type Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
 
 function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
   return (
