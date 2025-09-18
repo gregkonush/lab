1668cad5c5a8c89c5ffed489886b1db92e5db380
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2025-09-18 12:21:50 -0700

    feat: add infra

---
+++ scripts/build-miel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+IMAGE_NAME="kalmyk.duckdns.org/lab/miel"
+DOCKERFILE="services/miel/Dockerfile"
+CONTEXT_PATH="."
+
+if [ $# -eq 1 ]; then
+  TAG=$1
+else
+  TAG=$(date +"%Y%m%d_%H%M%S")
+fi
+
+FULL_IMAGE_NAME="${IMAGE_NAME}:${TAG}"
+
+echo "Building and pushing ${FULL_IMAGE_NAME}"
+
+docker buildx build \
+  --platform linux/arm64 \
+  -t "${FULL_IMAGE_NAME}" \
+  -f "${DOCKERFILE}" \
+  "${CONTEXT_PATH}" \
+  --push
+
+echo "Image pushed: ${FULL_IMAGE_NAME}"
