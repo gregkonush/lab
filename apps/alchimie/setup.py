5437f7510fc4a03c9e92dfac07e37fbcfb0ce2ed
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2025-09-19 21:43:59 -0700

    feat: update alchimie to the latest

--- apps/alchimie/setup.py
+++ apps/alchimie/setup.py
@@ -4,8 +4,16 @@ setup(
     name="alchimie",
     packages=find_packages(exclude=["alchimie_tests"]),
     install_requires=[
-        "dagster",
-        "dagster-cloud"
+        "dagster==1.11.11",
+        "dagster-cloud==1.11.11",
+        "dagster-k8s==0.27.11",
+        "psycopg2-binary==2.9.10",
+        "polars==1.33.1",
     ],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
+    extras_require={
+        "dev": [
+            "dagster-webserver==1.11.11",
+            "pytest==8.4.2",
+        ]
+    },
 )
