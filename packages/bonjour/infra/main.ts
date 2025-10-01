7efd2619799df8f7237b6ff8aa4ff42775e16924
Greg Konush <12027037+gregkonush@users.noreply.github.com>
2025-10-01 00:48:27 -0700

    fix: simplify plugin, update manifests

--- packages/bonjour/infra/main.ts
+++ packages/bonjour/infra/main.ts
@@ -1,9 +1,8 @@
 import { App } from "cdk8s";
 import { ServerChart } from "./server-chart";
 
-const image =
-  process.env.IMAGE ?? "registry.ide-newton.ts.net/lab/bonjour:latest";
-const namespace = process.env.NAMESPACE ?? "default";
+const image = "registry.ide-newton.ts.net/lab/bonjour:latest";
+const namespace = "bonjour";
 const replicas = Number.parseInt(process.env.REPLICAS ?? "2", 10);
 const port = Number.parseInt(process.env.PORT ?? "3000", 10);
 const cpuTarget = Number.parseInt(process.env.CPU_TARGET_PERCENT ?? "70", 10);
